9528dd4013aeadfce6add1a90f121c254b9687be
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-01-08 13:49:17 +0000

    Remove no longer needed commented old render logic

--- src/index.tsx
+++ src/index.tsx
@@ -31,11 +31,6 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

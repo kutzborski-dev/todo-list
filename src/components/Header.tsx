5839c54a343e3a7d9d79ce7e9ef4f31e9071ee7d
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-01-13 17:25:51 +0000

    (#2) Add MUI component/icon library, add settings button and apply basic rotating animation effect on hover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
+import SettingsIcon from '@mui/icons-material/Settings';
+
 function Header() {
     return (
-        <header className="py-5 px-6 bg-white shadow-md shadow-slate-800/5">
-            <h1 className="uppercase font-bold text-slate-700 text-lg">Todo List App</h1>
+        <header className="py-5 px-6 bg-white shadow-md shadow-slate-800/5 relative">
+            <h1 className="text-center uppercase font-bold text-slate-700 text-lg flex-1">Todo List App</h1>
+            <button className="absolute top-0 bottom-0 my-auto right-6 h-6 w-6 leading-none rotate-0 transition-transform duration-300 hover:rotate-180">
+                <SettingsIcon fontSize="medium" className="text-slate-700" />
+            </button>
         </header>
     );
 }

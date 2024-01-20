d0fb24245508e3cf3186099d235e4d382370a2d2
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-01-20 20:25:20 +0000

    (#2) Add basic further styling and action buttons (e.g. delete/edit)

--- src/pages/Todo/components/TodoItem.tsx
+++ src/pages/Todo/components/TodoItem.tsx
@@ -1,6 +1,9 @@
 function TodoItem({todo}: {todo: any}) {
     return (
-        <li>{todo.label}</li>
+        <li>
+            <span className="w-3 h-3 bg-[#ff0000] inline-block mr-2 rounded-full"></span>
+            {todo.label}
+        </li>
     )
 }
 
--- src/pages/Todo/components/TodoList.tsx
+++ src/pages/Todo/components/TodoList.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@mui/material";
 import TodoItem from "./TodoItem";
+import { DoneAll as DoneAllIcon, DeleteForever as DeleteForeverIcon, EditNote as EditNoteIcon } from '@mui/icons-material';
 
 function TodoList({todos}: {todos: Array<object>}) {
     return (
         <div id="todo-list-container">
-            <h3>Todo List Name</h3>
+            <div id="todo-list-header" className="flex">
+                <div className="flex flex-1">
+                    <div className="mr-2">
+                        <DoneAllIcon fontSize="medium" className="text-slate-700" />
+                    </div>
+                    <h3>Todo List Name</h3>
+                </div>
+                <div>
+                    <Button variant="text">
+                        <EditNoteIcon fontSize="medium" className="text-slate-700" />
+                    </Button>
+
+                    <Button variant="text">
+                        <DeleteForeverIcon fontSize="medium" className="text-red-500" />
+                    </Button>
+                </div>
+            </div>
 
             <ul id="todo-list">
                 {todos.map(todo => <TodoItem todo={todo} />)}

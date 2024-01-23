0452ab3b0d5d215763ddf545dedc85ee379d9d45
Andre <andre.kutzborski@sequelgroup.co.uk>
2024-01-23 16:32:04 +0000

    (#2) Double distance between settings and theme mode button, previously a bit too tight

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
         <header className="py-5 px-6 bg-white dark:bg-slate-700/50 shadow-md shadow-slate-800/5 dark:shadow-none relative">
             <h2 className="text-center uppercase font-bold text-slate-700 dark:text-slate-400 text-lg flex-1"><a href="/">Todo List App</a></h2>
             <div className="absolute top-0 bottom-0 right-6 my-auto h-6">
-                <button className="mr-2" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+                <button className="mr-4" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
                     {
                         theme === 'dark' ?
                             <LightModeOutlinedIcon fontSize="medium" className="text-slate-700 dark:text-slate-400" />

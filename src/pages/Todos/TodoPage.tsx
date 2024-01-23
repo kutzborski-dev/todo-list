c67256f69e6d520f98d77a6ccdf911a3878f170e
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-01-23 19:50:47 +0000

    (#2) Move logic related to the entire todo page rather than just the todo list from the list component up to the page component

--- src/pages/Todos/TodoPage.tsx
+++ src/pages/Todos/TodoPage.tsx
@@ -1,10 +1,16 @@
 import TodoList from "./components/TodoList";
 import { useParams } from "react-router-dom";
+import { 
+    DoneAll as DoneAllIcon,
+    Settings as SettingsIcon,
+    EditNote as EditNoteIcon
+} from '@mui/icons-material';
+import { Button } from "@mui/material";
 
 function TodoPage() {
     const { id: listId } = useParams();
 
-    if(!listId) return <h3 className="text-2xl leading-6 text-center">"Todo list not found"</h3>;
+    if(!listId) return <h3 className="text-2xl leading-6 text-center">Todo list not found</h3>;
     
     const todoDummyData = [{
         label: "Test",
@@ -18,7 +24,29 @@ function TodoPage() {
     }];
     return (
         <main>
-            <TodoList todos={todoDummyData} />
+            <div id="todo-list-container" className="mb-4">
+                <div id="todo-list-header" className="flex">
+                    <div className="flex flex-1 mt-1 mb-6">
+                        <div className="mr-2">
+                            <DoneAllIcon fontSize="medium" className="text-slate-400" />
+                        </div>
+                        <h1 id="todo-list-name" className="text-2xl leading-6">Todo List Name</h1>
+                    </div>
+                    <div>
+                        <Button>
+                            <EditNoteIcon fontSize="small" className="text-slate-600 mr-1" />
+                            <label className="text-slate-700 cursor-pointer">Edit</label>
+                        </Button>
+
+                        <Button>
+                            <SettingsIcon fontSize="small" className="text-slate-600 mr-1" />
+                            <label className="text-slate-700 cursor-pointer">Settings</label>
+                        </Button>
+                    </div>
+                </div>
+
+                <TodoList todos={todoDummyData} />
+            </div>
         </main>
     )
 }
--- src/pages/Todos/components/TodoList.tsx
+++ src/pages/Todos/components/TodoList.tsx
@@ -1,38 +1,10 @@
-import { Button } from "@mui/material";
 import TodoItem from "./TodoItem";
-import { 
-    DoneAll as DoneAllIcon,
-    Settings as SettingsIcon,
-    EditNote as EditNoteIcon
-} from '@mui/icons-material';
 
 function TodoList({todos}: {todos: Array<object>}) {
     return (
-        <div id="todo-list-container" className="mb-4">
-            <div id="todo-list-header" className="flex">
-                <div className="flex flex-1 mt-1 mb-6">
-                    <div className="mr-2">
-                        <DoneAllIcon fontSize="small" className="text-slate-400" />
-                    </div>
-                    <h3 id="todo-list-name" className="text-xl leading-6">Todo List Name</h3>
-                </div>
-                <div>
-                    <Button>
-                        <EditNoteIcon fontSize="small" className="text-slate-600 mr-1" />
-                        <label className="text-slate-700 cursor-pointer">Edit</label>
-                    </Button>
-
-                    <Button>
-                        <SettingsIcon fontSize="small" className="text-slate-600 mr-1" />
-                        <label className="text-slate-700 cursor-pointer">Settings</label>
-                    </Button>
-                </div>
-            </div>
-
-            <ul id="todo-list">
-                {todos.map(todo => <TodoItem todo={todo} />)}
-            </ul>
-        </div>
+        <ul id="todo-list">
+            {todos.map(todo => <TodoItem todo={todo} />)}
+        </ul>
     )
 }
 

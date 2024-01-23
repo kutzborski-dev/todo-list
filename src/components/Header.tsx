56efb54ebfb351b3ed50acc5bc7e5bbe270769fd
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-01-23 00:06:48 +0000

    (#2) Turn header site title into a link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import SettingsIcon from '@mui/icons-material/Settings';
 function Header() {
     return (
         <header className="py-5 px-6 bg-white dark:bg-slate-700/50 shadow-md shadow-slate-800/5 dark:shadow-none relative">
-            <h1 className="text-center uppercase font-bold text-slate-700 text-lg flex-1">Todo List App</h1>
-            <button className="absolute top-0 bottom-0 my-auto right-6 h-6 w-6 leading-none rotate-0 transition-transform duration-300 hover:rotate-180">
+            <h1 className="text-center uppercase font-bold text-slate-700 text-lg flex-1"><a href="/">Todo List App</a></h1>
+            <a href="/settings" className="absolute top-0 bottom-0 my-auto right-6 h-6 w-6 leading-none rotate-0 transition-transform duration-300 hover:rotate-180">
                 <SettingsIcon fontSize="medium" className="text-slate-700" />
-            </button>
+            </a>
         </header>
     );
 }

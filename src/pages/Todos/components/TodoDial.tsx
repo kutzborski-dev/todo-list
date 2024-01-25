64c77ffeb4a9d0f3756bf68b21a7ee677fca5d40
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-01-25 14:55:41 +0000

    (#2) Add href prop to enable using the dial component as a link

--- src/pages/Todos/components/TodoDial.tsx
+++ src/pages/Todos/components/TodoDial.tsx
@@ -1,7 +1,8 @@
 import { SpeedDial  } from "@mui/material";
 import Tooltip from "@mui/material/Tooltip";
 
-export default function TodoDial({ariaLabel = "", tooltipTitle = "", positionStyle = null, icon = null}: {ariaLabel?: string, tooltipTitle?: string, positionStyle?: object | null, icon?: React.ReactNode}) {
+export default function TodoDial({ariaLabel = "", tooltipTitle = "", href = null, positionStyle = null, icon = null}:
+{ariaLabel?: string, tooltipTitle?: string, href?: null | string, positionStyle?: object | null, icon?: React.ReactNode}) {
     const defaultPosition = { position: 'absolute', bottom: 30, right: 30 };
 
     if(!positionStyle) {
@@ -10,17 +11,30 @@ export default function TodoDial({ariaLabel = "", tooltipTitle = "", positionSty
         positionStyle = {...defaultPosition, ...positionStyle};
     }
 
-    return (
+    const Dial = () => (
         <Tooltip title={tooltipTitle.length ? tooltipTitle : ariaLabel}>
-            <SpeedDial
-                ariaLabel={ariaLabel}
-                sx={positionStyle}
-                icon={icon}
-                FabProps={{
-                    className: "!bg-primary hover:!bg-primary-hover dark:!bg-primary-dark dark:hover:!bg-primary-dark-hover",
-                }}
-            >
-            </SpeedDial>
+                <SpeedDial
+                    ariaLabel={ariaLabel}
+                    sx={positionStyle}
+                    icon={icon}
+                    FabProps={{
+                        className: "!bg-primary hover:!bg-primary-hover dark:!bg-primary-dark dark:hover:!bg-primary-dark-hover",
+                    }}
+                >
+                </SpeedDial>
         </Tooltip>
+    );
+
+    return (
+        <>
+            {
+                href ?
+                    <a href={href} aria-label={ariaLabel}>
+                        <Dial />
+                    </a>
+                :
+                    <Dial />
+            }
+        </>
     );
 }

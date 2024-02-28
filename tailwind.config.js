f8f56fdaa5187d317346180b619e02a2dce4ad7d
Andre Kutzborski <andre_kutzborski@outlook.com>
2024-02-28 01:26:49 +0000

    (#2) Install NextUI to replace MaterialUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors');
+const {nextui} = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -22,5 +24,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 }

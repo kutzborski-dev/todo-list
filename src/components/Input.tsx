82ded8a38e741afc3fb2344a185b625f9a20bfb7
Andre Kutzborski <andre.kutzborski@gmail.com>
2024-10-25 02:24:31 +0100

    Replace TextInput with new custom Input component via NextUI

---
+++ src/components/Input.tsx
@@ -0,0 +1,32 @@
+import { Input as NextUIInput } from "@nextui-org/react";
+
+type InputProps = {
+    id?: string;
+    type?: string;
+    radius?: string;
+    label: string;
+    name?: string;
+    className?: string;
+    classNames?: {} | {[key: string]: string}
+}
+
+export default function Input({id = '', type = 'text', label, name = '', className = '', classNames = {}}: InputProps) {
+    classNames = {
+        ...{
+            inputWrapper: "!bg-transparent !shadow-none border-b-1 border-accent",
+            label: "text-lg"
+        }
+    };
+
+    return (
+        <NextUIInput
+            id={id ?? ''}
+            radius="none"
+            type={type ?? 'text'}
+            label={label}
+            name={name ?? ''}
+            className={className ?? ''}
+            classNames={classNames}
+        />
+    )
+}

10ea4d278c55396b9ab1790d44482b603104ed38
Andre Kutzborski <andre.kutzborski@gmail.com>
2024-10-25 02:23:14 +0100

    Extend theme by adding new colour options

--- src/theme.js
+++ src/theme.js
@@ -3,11 +3,15 @@ const twColors = require('tailwindcss/colors');
 export const light = {
   foreground: twColors.white,
   primary: twColors.sky[600],
-  secondary: twColors.sky[500]
+  secondary: twColors.sky[500],
+  accent: twColors.slate[300],
+  body: twColors.slate[700]
 }
 
 export const dark = {
   foreground: twColors.slate[700],
   primary: twColors.violet[500],
-  secondary: twColors.violet[600]
+  secondary: twColors.violet[600],
+  accent: twColors.slate[300],
+  body: twColors.slate[300]
 }

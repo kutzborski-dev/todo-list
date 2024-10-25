2f1e78de565bee8de9abc3e5cec40cced8986240
Andre Kutzborski <andre.kutzborski@gmail.com>
2024-10-25 02:25:17 +0100

    Replace material UI button with NextUI and update styling accordingly; In progress lists now show an orange double tick

--- src/pages/Todos/components/TodoTab.tsx
+++ src/pages/Todos/components/TodoTab.tsx
@@ -1,22 +1,21 @@
 import { DoneAll as DoneAllIcon } from "@mui/icons-material";
 import colors from 'tailwindcss/colors';
-import { Button } from "@mui/material";
 import TodoListType from "../types/TodoListType";
 import { useThemeContext } from "context/themeContext";
+import { Link, Button } from "@nextui-org/react";
 
 function TodoTab({list}: {list: TodoListType}) {
     const { theme } = useThemeContext();
     const listItemsCompleteCount = list.data.filter(todo => todo.status === 'complete').length;
 
     return (
-        <Button href={`/list/${list.id}`} style={{
-            color: theme === 'dark' ? colors.slate[300] : colors.slate[600],
-            textTransform: 'inherit'
-        }}>
-            <div className="mr-2">
-                <DoneAllIcon fontSize="small" className={list.status === 'complete' ? 'text-lime-500 dark:text-green-400' : 'text-slate-500'} />
-            </div>
-            <h1 className="text-xl leading-7">{list.name}</h1>
+        <Button
+            as={Link}
+            href={`/list/${list.id}`}
+            className="bg-transparent hover:!no-underline"
+            startContent={<DoneAllIcon fontSize="small" className={list.status === 'complete' ? 'text-lime-500 dark:text-green-400' : list.status === 'inprogress' ? 'text-orange-400' : 'text-slate-500'} />}
+        >
+            <h1 className="text-xl leading-7 text-body">{list.name}</h1>
             <span className={`ml-1 ${list.status === 'complete' ? 'text-lime-500 dark:text-green-400' : 'text-slate-400 dark:text-slate-500'}`}>({listItemsCompleteCount}/{list.data.length} items done)</span>
         </Button>
     )
